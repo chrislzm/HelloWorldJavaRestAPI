527efed4631eeef694a0569abfd556908d7155f5
chrislzm <me@chrisleung.com>
2017-12-01 18:23:10 -0800

    Initial commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,28 @@
 package hello;
 
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Resource controller (handles HTTP RESTful web service requests)
+ */
+@RestController
 public class GreetingController {
 
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    /**
+     * Handles all GET requests for /greeting
+     * @param name URL query string parameter "name"
+     * @return Greeting object (customized with name if one was provided, otherwise default)
+     */
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
 }
